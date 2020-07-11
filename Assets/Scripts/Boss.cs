32f74debb2c1ae44275eb053ddcc7f03e3395459
Tizeng Yan <yan235689147@gmail.com>
2020-07-11 22:12:32 +0800

    new boss movement

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -16,7 +16,22 @@ public class Boss : MonoBehaviour {
     float timeCount = 0f;
 
     void Start() {
+        StartCoroutine("RotatePattern2");
+    }
 
+    IEnumerator RotatePattern1() {
+        while (true) {
+            isClockWise = !isClockWise;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    IEnumerator RotatePattern2() {
+        while (true) {
+            float delay = Random.Range(1f, 5f);
+            isClockWise = !isClockWise;
+            yield return new WaitForSeconds(delay);
+        }
     }
 
     void Update() {
@@ -33,8 +48,12 @@ public class Boss : MonoBehaviour {
         else if(transform.position.x < leftPosX) {
             isMoveRight = true;
         }
-        // rotate
-        timeCount += Time.deltaTime * rotateSpeed;
-        transform.rotation = Quaternion.AngleAxis(timeCount, Vector3.forward);
+        // rotate by z axis
+        if (isClockWise)
+            timeCount += Time.deltaTime * rotateSpeed;
+        else
+            timeCount -= Time.deltaTime * rotateSpeed;
+        //transform.rotation = Quaternion.AngleAxis(timeCount, Vector3.forward);
+        transform.rotation = Quaternion.Euler(0f, 0f, timeCount);
     }
 }
